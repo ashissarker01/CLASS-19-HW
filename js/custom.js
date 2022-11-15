59f64bb1379dc491d00ef6ed63a596485036bda3
ASHIS SARKER <114518689+ashissarker01@users.noreply.github.com>
2022-11-16 03:50:50 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,80 @@
+
+$('.sliders_part').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow:'<i class="fa-regular fa-circle-left prev fa-3x"></i>',
+    nextArrow:'<i class="fa-regular fa-circle-right next fa-3x"></i>',
+    dots:true,
+    
+  });
+
+
+
+
+
+
+
+
+
+  $('.car_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2700,
+    arrows: true,
+
+    prevArrow:'<i class="fa-solid fa-circle-chevron-left carprev fa-3x"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-chevron-right carnext fa-3x"></i>',
+    dots:true,
+    
+  });
+
+  
+
+
+  $('.bike_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2200,
+    arrows: true,
+
+    prevArrow:'<i class="fa-solid fa-circle-chevron-left carprev fa-3x"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-chevron-right carnext fa-3x"></i>',
+    dots:true,
+    
+  });
+
+
+
+
+  
+  $('.cycle_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2400,
+    arrows: true,
+
+    prevArrow:'<i class="fa-solid fa-circle-chevron-left carprev fa-3x"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-chevron-right carnext fa-3x"></i>',
+    dots:true,
+    
+  });
+
+
+  $('.boat_slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+
+    prevArrow:'<i class="fa-solid fa-circle-chevron-left carprev fa-3x"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-chevron-right carnext fa-3x"></i>',
+    dots:true,
+    
+  });
